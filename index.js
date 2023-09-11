f656d12a37d6bab65e0da6006f366043d3d8ca57
Marcin Kwietniewski <kwietniewski.marcin@gmail.com>
2023-09-10 23:46:51 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+(function() {
+    'use strict';
+
+    function showContent() {
+        document.getElementById('LoadMessage').style.display = 'none';
+        $('#block_menu')[0].style.visibility = 'visible';
+    }
+
+    // Your code here...
+    function loadCompManagerData(data) {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(data, 'text/html');
+        var additionalContent = doc.body.innerHTML;
+        document.body.insertAdjacentHTML('beforeend', additionalContent);
+        showContent();
+    }
+
+    var compMgrResponse;
+    fetch('https://dance-comp-improver-5248baad7892.herokuapp.com/https://www.comp-mngr.com/northstar2023/NorthStar2023_HeatLists.htm', {
+        //'https://cors-anywhere.herokuapp.com/https://www.comp-mngr.com/northstar2023/NorthStar2023_HeatLists.htm', {
+        method: 'GET',
+        headers: {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'en-US,en;q=0.9,pl;q=0.8',
+            'Host': 'www.comp-mngr.com',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+        }
+    })
+    .then(response => response.text())  // or response.json() if the response is JSON
+    .then(data => {
+        console.log(data);
+        loadCompManagerData(data);
+
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+})();

dc9c8aa6831e6ac504c537e2605bd13658a59794
Marcin Kwietniewski <kwietnie@amazon.com>
2026-02-04 22:48:52 -0500

    switch proxy url to cors-proxy

--- index.js
+++ index.js
@@ -36,7 +36,7 @@
     }
     
     var compMgrResponse;
-    fetch('https://dance-comp-improver-5248baad7892.herokuapp.com/https://www.comp-mngr.com/' + compString.toLowerCase() + '/' + compString + '_HeatLists.htm', {
+    fetch('https://dance-comp-improver-5248baad7892.herokuapp.com/cors-proxy/https://www.comp-mngr.com/' + compString.toLowerCase() + '/' + compString + '_HeatLists.htm', {
         //'https://dance-comp-improver-5248baad7892.herokuapp.com/https://www.comp-mngr.com/canamgala23/canamgala23_HeatLists.htm', {
         //'https://cors-anywhere.herokuapp.com/https://www.comp-mngr.com/northstar2023/NorthStar2023_HeatLists.htm', {
         method: 'GET',
